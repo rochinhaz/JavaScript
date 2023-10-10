1eca59d6659049b9b5ee09b1b38563a7f8920152
Jeferson Rocha <jeferson_rocha57@hotmail.com>
2023-10-10 13:41:20 -0300

    aula14pt1

--- Aula13/teste.js
+++ Aula13/teste.js
@@ -1,5 +1,20 @@
+for(c=1;c<=6;c++){
+    console.log(`Passo ${c}`)
+}
+
+
+/*
+var c=1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c<=6)
+*/
+
+/*
 var c = 1
-while(c<6){
+while(c<=6){
     console.log('tudo bem?')
     c++
 }
+*/
